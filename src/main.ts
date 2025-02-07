48527178ef2e9587f258889d41e2f129003d204d
mrBidone <ab.bz@ukr.net>
2025-02-07 07:47:41 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,14 +13,79 @@ console.log(add(1, 2));
 //   age: 30,
 // };
 
-type User = {
-  name: string;
-  age: number;
-};
+// type User = {
+//   name: string;
+//   age: number;
+// };
 
-let user: User = {
-  name: "John",
-  age: 25,
-};
+// let user: User = {
+//   name: "John",
+//   age: 25,
+// };
 
-console.log(user);
+// console.log(user);
+
+// const userName: string = "Andrii";
+// const lastName: string = "Bidenko";
+// const userAge: number = 34;
+// const isOnline: boolean = false;
+// const budget: null = null;
+// const status: undefined = undefined;
+
+// type userProfile = {
+//   userName: string;
+//   age: number;
+//   isActive: boolean;
+// };
+
+// const displayUserProfile = (user: userProfile) => {
+//   return `User name is ${user.userName} he is ${user.age} years old and he is ${user.isActive}`;
+// };
+
+// const user = {
+//   userName: "Bob",
+//   age: 35,
+//   isActive: true,
+// };
+// console.log(displayUserProfile(user));
+
+// const logName = (name: string) => {
+//   return `${name}`;
+// };
+
+// console.log(logName("Andrii"));
+
+// const calcTotal = (arr: number[]) => {
+//   return arr.reduce((acc, el) => acc + el, 0);
+// };
+
+// const numbersArray = [10, 20, 30, 40];
+// console.log(calcTotal(numbersArray));
+
+// type User = {
+//   name: string;
+//   age: number;
+// };
+
+// const userListitem = (arr: User[]) => {
+//   return arr.map((item) => item);
+// };
+
+// const userList = [
+//   {
+//     name: "Bob",
+//     age: 25,
+//   },
+//   {
+//     name: "John",
+//     age: 34,
+//   },
+//   {
+//     name: "Andrii",
+//     age: 36,
+//   },
+// ];
+
+// console.log(userListitem(userList));
+
+// UNKNOWN===========================
